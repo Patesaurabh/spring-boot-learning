425e8cd966604f7a476294f02f24e304b5511c53
Saurabh Pate <saurabhdp@softcell.com>
2021-06-01 09:56:19 +0530

    new module added

---
+++ learning-startup/src/main/java/com/learning/startup/LearningStartupApplication.java
@@ -0,0 +1,11 @@
+package com.learning.startup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningStartupApplication {
+    public static void main(String[] args){
+        SpringApplication.run(LearningStartupApplication.class, args);
+    }
+}
